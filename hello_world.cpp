d6de5df8d551a21b9b974fcd331ba21db9104804
prasun kumar <91335510+thunderxies@users.noreply.github.com>
2021-10-29 20:52:51 +0530

    Add files via upload

---
+++ hello_world.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+int main() {
+cout<<"hello world"<<endl<<"this is"<<endl<<" prasun"<<endl<<"cse student";
+return 0;
+}
---
+++ practicecin.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+ {
+  int x=10,y=20;
+  cout<<x<<" "<<y;
+  
+  return 0;
+}
